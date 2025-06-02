f62a4b27a458eaa252a5f2c8721277b744b54851
DebMs123 <142893580+DebMs123@users.noreply.github.com>
2025-06-03 01:32:27 +0530

    Update greetings.cs

--- tools/greetings.cs
+++ tools/greetings.cs
@@ -1,6 +1,4 @@
 using System.ComponentModel;
-using MCP.HTTP.Server.Entities;
-using MCP.HTTP.Server.Services;
 using ModelContextProtocol.Server;
 
 namespace McpServer.Tools;
@@ -14,6 +12,6 @@ public class GreetingTools
     [McpServerTool, Description("Counts the number of words in the input message.")] 
     public async Task<int?> WordCount([Description("The input")] string message)
     {
-        return await message.Split(new[] { ' ', '\t', '\n' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        return message.Split(new[] { ' ', '\t', '\n' }, StringSplitOptions.RemoveEmptyEntries).Length;
     }
 }
